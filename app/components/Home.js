bfcbf4d5a68ec2937dac05038579acbb93cafe34
blakeparkinson <wizardplow@gmail.com>
2016-12-06 22:06:15 -0700

    adding the router stuff

---
+++ app/components/Home.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Home = React.createClass({
+  render: function () {
+    return (
+      <div className="home-container"> Hello from Home! </div>
+    )
+  }
+});
+
+module.exports = Home;
