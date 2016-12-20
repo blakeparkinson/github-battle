fbbe0b85d7805284c0e3a5ce833e62b0794f34b7
blakeparkinson <wizardplow@gmail.com>
2016-12-19 21:33:42 -0700

    folder restructuring

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + '/app/index.html',
+  template: __dirname + '/client/app/index.html',
   filename: 'index.html',
   inject: 'body'
 });
 
 module.exports = {
   entry: [
-    './app/index.js'
+    './client/app/index.js'
   ],
   output: {
-    path: __dirname + '/dist',
+    path: __dirname + '/client/dist',
     filename: "index_bundle.js"
   },
   module: {
     loaders: [
-      {test: /\.js$/, include: __dirname + '/app', loader: "babel-loader"},
+      {test: /\.js$/, include: __dirname + '/client/app', loader: "babel-loader"},
       { test: /\.css$/, loader: "style-loader!css-loader" }
     ]
   },
